4f86b3d1cb8dfbaa29c0e02b40f9b87655632607
Adil Anwar Sadath <83989883+AdilAnwarSadath@users.noreply.github.com>
2026-07-07 19:44:59 +0530

    Solve huber-loss

---
+++ huber-loss/huber-loss.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def huber_loss(y_true, y_pred, delta=1.0):
+    """
+    Compute Huber Loss for regression.
+    """
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    e = np.abs(y_true - y_pred)
+
+    res = np.where(e<=delta, 0.5*e**2 , delta*(e-0.5*delta))
+    return np.mean(res)
+    
